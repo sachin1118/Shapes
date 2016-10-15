74d1ef3a9394aabc5f561e2d658fa5d9a31761c4
Unknown <sachin_1118@yahoo.com>
2016-10-15 13:32:28 -0400

    added ourprint and square functions

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		ourprint("hello world");
+		ourprint("hello Sachin");
+		System.out.println(square(2));
+	}
+
+	public static void ourprint(String msg) {
+		System.out.println("Now we're going to print: ");
+		System.out.println(msg);
+	}
+
+	public static double square(double x) {
+		// return x * x;
+		return Math.pow(x, 3);
 	}
 }
