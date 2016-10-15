2fd9e5297dcd5200625d6e706cf319e2817e5b09
Unknown <sachin_1118@yahoo.com>
2016-10-15 14:03:16 -0400

    Added min and max functions

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,45 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		int x = 3;
+		x = 2;
+		if (x == 0) {
+			System.out.println("x is 0");
+		} else if (x == 1) {
+			System.out.println("x is 1");
+		} else {
+			System.out.println("x is neither 0 nor 1");
+		}
 		ourprint("hello world");
 		ourprint("hello Sachin");
-		System.out.println(square(2));
+		System.out.println(cube(2));
+		System.out.println(max(2, 1));
+		System.out.println(min(10, 6));
 	}
 
 	public static void ourprint(String msg) {
-		System.out.println("Now we're going to print: ");
+		System.out.print("Now we're going to print: ");
 		System.out.println(msg);
 	}
 
-	public static double square(double x) {
+	public static double cube(double x) {
 		// return x * x;
 		return Math.pow(x, 3);
+	}
+
+	public static double max(double a, double b) {
+		if (a > b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+	
+	public static double min(double a, double b) {
+		if (a < b) {
+			return a;
+		} else {
+			return b;
+		}
 	}
 }
